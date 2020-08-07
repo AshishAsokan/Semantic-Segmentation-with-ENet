183415cf74421c79040ad35b2d4bb2a4e9a07cb6
AshishAsokan <raashish020@gmail.com>
2020-08-07 23:01:24 +0530

    [ADDED] Regular BN and tests

--- enet_seg/enet_blocks.py
+++ enet_seg/enet_blocks.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from enet_seg.utilities import MaxUnpool2D
 import numpy as np
 
 class InitialBlock(tf.keras.layers.Layer):
@@ -53,7 +54,7 @@ class InitialBlock(tf.keras.layers.Layer):
 
 class DownsampleBN(tf.keras.layers.Layer):
 
-    def __init__(self, out_channel : int, filter_size : int = 3, pool_size : tuple = (2, 2), pool_stride : tuple = (2, 2), bn_pad : str = 'SAME'):
+    def __init__(self, out_channel : int, filter_size : int = 3, pool_size : tuple = (2, 2), pool_stride : tuple = (2, 2), bn_pad : str = 'SAME', p_dropout : float = 0.1):
 
         """
         Downsampling Bottleneck block of the E-Net architecture.
@@ -134,15 +135,164 @@ class DownsampleBN(tf.keras.layers.Layer):
         output = tf.keras.layers.PReLU()(output)
         return output
 
+class RegularBN(tf.keras.layers.Layer):
 
-class UpsampleBN(tf.keras.Model):
+    def __init__(self, filter_size : int = 3,  bn_pad : str = 'SAME', p_dropout : float = 0.1, filters : int = 4):
 
-    def __init__(self):
+        """
+        Regular Bottleneck block of the E-Net architecture.
+
+        Parameters:
+
+            bn_pad : Either 'SAME' or 'VALID' for pooling layer and conv in BRANCH-2
+            out_channel : No of channels in output
+            filter_size : Filter size for the conv operation in BRANCH-2
+            p_dropout : Dropout rate of the regularizer
+
+        Returns:
+
+            output : Result of the element-wise addition of the 2 branch outputs with PReLU activation
+
+        """
 
         super().__init__()
+        self.filter_size = filter_size
+        self.bn_pad = bn_pad
+        self.filters = filters
 
-    def call():
-        raise NotImplementedError
+        # PReLU layer
+        self.b2_prelu = tf.keras.layers.PReLU()
+
+        # Regularizer : Spatial Dropout with p = 0.01
+        self.b2_reg = tf.keras.layers.SpatialDropout2D(rate = p_dropout, data_format = 'channels_last')
+
+        # Batch normalization
+        self.b2_batch_norm = tf.keras.layers.BatchNormalization()
+
+    def build(self, input_shape):
+
+        # Number of output channels
+        channels_out = input_shape[3]
+
+        # 1x1 convolution and conv of choice
+        self.b2_conv1 = tf.keras.layers.Conv2D(filters = self.filters, kernel_size = (1, 1), strides = (1, 1))
+        self.b2_conv2 = tf.keras.layers.Conv2D(filters = self.filters, kernel_size = (self.filter_size, self.filter_size), strides = (1, 1), padding = self.bn_pad)
+
+        # 1x1 expansion layer after convolution
+        self.b2_expand = tf.keras.layers.Conv2D(filters = channels_out, kernel_size = (1, 1), strides = (1, 1))
+
+    def call(self, block_input):
+        
+        ######## BRANCH - 1 ###############
+
+        # Storing input as Branch-1
+        branch_1 = block_input
+
+        ######## BRANCH - 2 ###############
+
+        # 1x1 projection with BN and PReLU
+        conv1_out = self.b2_conv1(block_input)
+        batch1_out = self.b2_batch_norm(conv1_out)
+        prelu1_out = self.b2_prelu(batch1_out)
+
+        # 2nd convolution with BN and PReLU
+        conv2_out = self.b2_conv2(prelu1_out)
+        batch2_out = self.b2_batch_norm(conv2_out)
+        prelu2_out = self.b2_prelu(batch2_out)
+
+        # 1x1 expansion and spatial dropout
+        exp1_out = self.b2_expand(prelu2_out)
+        branch_2 = self.b2_reg(exp1_out)
+
+        print(branch_2.shape)
+
+        # Combining outputs of the 2 branches
+        output = tf.keras.layers.Add()([branch_1, branch_2])
+        output = tf.keras.layers.PReLU()(output)
+        return output
+
+class UpsampleBN(tf.keras.layers.Layer):
+
+    def __init__(self, out_channel : int, filter_size : int = 3, pool_size : tuple = (2, 2), pool_stride : tuple = (2, 2), bn_pad : str = 'SAME', p_dropout : float = 0.1):
+
+        """
+        Downsampling Bottleneck block of the E-Net architecture.
+
+        Parameters:
+
+            pool_size : Tuple for pooling filter size
+            pool_stride : Strides in both directions for pooling layer
+            bn_pad : Either 'SAME' or 'VALID' for pooling layer and conv in BRANCH-2
+            in_channel : No of channels in input
+            out_channel : No of channels in output
+            filter_size : Filter size for the conv operation in BRANCH-2
+
+        Returns:
+
+            output : Result of the element-wise addition of the 2 branch outputs with PReLU activation
+
+        """
+
+        super().__init__()
+        self.filter_size = filter_size
+        self.out_channel = out_channel
+        self.bn_pad = bn_pad
+
+        # Maxpool and PReLU layers
+        self.b1_maxpool = tf.keras.layers.MaxPool2D(pool_size = pool_size, strides = pool_stride, padding = bn_pad)
+        self.b2_prelu = tf.keras.layers.PReLU()
+        
+        # 1x1 expansion layer after convolution
+        self.b2_expand = tf.keras.layers.Conv2D(filters = out_channel, kernel_size = (1, 1), strides = (1, 1))
+
+        # Regularizer : Spatial Dropout with p = 0.01
+        self.b2_reg = tf.keras.layers.SpatialDropout2D(rate = 0.01, data_format = 'channels_last')
+
+        # Batch normalization
+        self.b2_batch_norm = tf.keras.layers.BatchNormalization()
+
+    def build(self, input_shape):
+
+        # Calculating the number of channels to pad
+        in_channel = input_shape[3]
+        pad = abs(in_channel - self.out_channel)
+        self.pad_tensor = tf.convert_to_tensor([[0, 0], [0, 0], [0, 0], [0, pad]], dtype = tf.int32)
+
+        # No. of filters for convolution
+        filters = self.out_channel // in_channel
+
+        # 2x2 convolution and conv of choice
+        self.b2_conv1 = tf.keras.layers.Conv2D(filters = filters, kernel_size = (2, 2), strides = (2, 2))
+        self.b2_conv2 = tf.keras.layers.Conv2D(filters = filters, kernel_size = (self.filter_size, self.filter_size), strides = (1, 1), padding = self.bn_pad)
+
+    def call(self, block_input):
+        
+        ######## BRANCH - 1 ###############
+
+        # Maxpool followed by padding the channels
+        maxpool_output = self.b1_maxpool(block_input)
+        branch_1 = tf.pad(maxpool_output, paddings = self.pad_tensor)
+
+        ######## BRANCH - 2 ###############
+
+        # 1st convolution with BN and PReLU
+        conv1_out = self.b2_conv1(block_input)
+        batch1_out = self.b2_batch_norm(conv1_out)
+        prelu1_out = self.b2_prelu(batch1_out)
+
+        # 2nd convolution with BN and PReLU
+        conv2_out = self.b2_conv2(prelu1_out)
+        batch2_out = self.b2_batch_norm(conv2_out)
+        prelu2_out = self.b2_prelu(batch2_out)
+
+        # 1x1 expansion and spatial dropout
+        exp1_out = self.b2_expand(prelu2_out)
+        branch_2 = self.b2_reg(exp1_out)
+
+        # Combining outputs of the 2 branches
+        output = tf.keras.layers.Add()([branch_1, branch_2])
+        output = tf.keras.layers.PReLU()(output)
+        return output
 
 class AntisymmetricBN(tf.keras.Model):
     def __init__(self):
@@ -153,15 +303,6 @@ class AntisymmetricBN(tf.keras.Model):
         raise NotImplementedError
 
 class DilatedBN(tf.keras.Model):
-
-    def __init__(self):
-
-        super().__init__()
-
-    def call():
-        raise NotImplementedError
-
-class RegularBN(tf.keras.Model):
 
     def __init__(self):
 
--- tests/test_enet_blocks.py
+++ tests/test_enet_blocks.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from enet_seg.enet_blocks import InitialBlock, DownsampleBN
+from enet_seg.enet_blocks import InitialBlock, DownsampleBN, RegularBN
 from enet_seg.utilities import MaxUnpool2D
 import pytest
 
@@ -11,7 +11,7 @@ def test_InitialBlock():
     tf.random.set_seed(1)
     test_input = tf.random.uniform(shape=[4, 512, 512, 3])
 
-    # Creating the layer and appling FFM
+    # Creating the layer and appling InitialBlock
     initial_block = InitialBlock()
     output = initial_block(test_input)
 
@@ -25,7 +25,7 @@ def test_DownsampleBN():
     tf.random.set_seed(1)
     test_input = tf.random.uniform(shape=[5, 256, 256, 16])
 
-    # Creating the layer and appling FFM
+    # Creating the layer and appling Downsample
     downsample_block = DownsampleBN(64)
     output = downsample_block(test_input)
 
@@ -42,12 +42,25 @@ def test_MaxUnpool2D():
     # Performing maxpool with argmax
     pool_output, pool_mask = tf.nn.max_pool_with_argmax(test_input, ksize = (2, 2), strides = (2, 2), padding = 'VALID')
 
-    # Creating the layer and appling FFM
+    # Creating the layer and appling Regular BN
     maxunpool = MaxUnpool2D(pool_mask = pool_mask)
     output = maxunpool(pool_output)
 
     assert output.shape == (5, 30, 30, 30)
 
+def test_RegularBN():
+
+    """ A simple test that checks the output of the Regular Bottleneck """
+
+    # Initializing random input
+    tf.random.set_seed(1)
+    test_input = tf.random.uniform(shape=[5, 128, 128, 64])
+
+    # Creating the layer and appling Regular BN
+    regular_bn = RegularBN()
+    output = regular_bn(test_input)
+
+    assert output.shape == (5, 128, 128, 64)
 
 if __name__ == '__main__':
     pytest.main([__file__])
